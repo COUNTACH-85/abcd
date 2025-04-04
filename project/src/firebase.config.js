5c7513d6cf38ecadd1480e025f246712ff818bc4
KONO-anupam <anupam.nitrkl@gmail.com>
2025-04-04 17:08:56 +0530

    added firebase auth

--- project/src/firebase.config.js
+++ project/src/firebase.config.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCj5isx2nwjywNZJEQoYVYKHckbBT0ixcQ",
-  authDomain: "iit-dhanbad-dd57d.firebaseapp.com",
-  projectId: "iit-dhanbad-dd57d",
-  storageBucket: "iit-dhanbad-dd57d.appspot.com",
-  messagingSenderId: "131773650590",
-  appId: "1:131773650590:web:5bdead7ae2b1e0c09b8d8f",
-  measurementId: "G-FWDKZRJZ0R"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
